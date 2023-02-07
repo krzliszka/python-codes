8fef0abf0451d10e4c3c3e3de3a23ac4316772f5
krzliszka <krz.liszka@gmail.com>
2023-02-07 12:26:00 +0100

    init commit, imports, order

--- networking-tools/wire_check.py
+++ networking-tools/wire_check.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 
+import os
+import random
+from os import geteuid, system
+from random import randrange
+
+import requests
+
+try:
+    from scapy.all import (ARP, ICMP, IP, TCP, UDP, Dot11, Dot11Beacon,
+                           Dot11Elt, Ether, RadioTap, RandIP, RandMAC, conf,
+                           promiscping, send, sendp, sr1, srp)
+except:
+    print('Module: >scapy< not found.')
+
+if int(geteuid()) != 0:
+    print("You must be a root to use this module")
+else:
+    pass
+
+conf.verb = 0  # ignoring scapy outputs
+
+
 class NetworkCheck:
     """
-    TODO123123
+    Description
     """
