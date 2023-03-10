2a779d9e77cbb8aa7e1d2a471e33eff37614d3d1
Krzysiek Liszka <krz.liszka@gmail.com>
2023-03-10 15:51:54 +0100

    Update wire_check.py

--- networking-tools/wire_check.py
+++ networking-tools/wire_check.py
@@ -80,10 +80,15 @@ class NetworkCheck:
         self.subnet = subnet
 
         # creating ARP request packets
-        pass
+        ans, un_ans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=self.subnet), iface=self.interface, timeout=2)
+        up_hosts = []
+        for snd, rcv in ans:
+            result = rcv.sprintf(r'"%ARP.psrc% %Ether.src%').split()
+            up_hosts.append(result)
+        return up_hosts
 
     # TCP stealth port scanning
-    def tcp_port_scanner(self):
+    def tcp_port_scanner(self, target_ip, start_point, end_point):
         """ Method for scanning range of TCP ports
             Example usage: nttl.tcpPortScanner(target_ip='192.168.1.1', start_point=1, end_point=100)
             This code should scan 1-100 range of ports
@@ -92,32 +97,90 @@ class NetworkCheck:
         self.start_point = start_point
         self.end_point = end_point
 
+        open_TCP_ports = []
+        for port in range(self.start_point, self.end_point):
+            # creating and sending syn packets
+            src_port = randrange(50000, 60000)
+            syn_pack = IP(dst=self.target_ip)/TCP(sport=src_port, dport=port, flags="S")
+            response = sr1(syn_pack, timeout=1, verbose=0)
+            if response:
+                if response[TCP].flags == 18:
+                    # if response is ACK, then send RST packets
+                    rst_pack = IP(DST=self.target_ip)/TCP(sport=src_port, dport=port, flags="R")
+                    response = send(rst_pack)
+                    open_TCP_ports.append(port)
+        return open_TCP_ports
+
     # UDP port scanning
-    def udp_port_scanner(self):
-        """Method for scanning range of UDP ports
-        
+    def udp_port_scanner(self, target_ip, start_point, end_point):
         """
-        pass
+        Method for scanning range of UDP ports
+        Example usage: nttl.udp_port_scanner
+        """
+        self.target_ip = target_ip
+        self.start_point = start_point
+        self.end_point = end_point
+
+        open_UDP_ports = []
+        for port in range(self.start_point, self.end_point):
+            # creating UDP packets and sending them to target IP
+            udp_packet = IP(dst=self.target_ip)/UDP(dport=port)
+            response = sr1(udp_packet, timeout=5, verbose=0)
+            if response == None:
+                open_UDP_ports.append(port)
+            else:
+                pass
+        return open_UDP_ports
 
     # enabling wireless monitoring
     def enable_monitor(self, interface):
         """
-
+        Method for enabling wireless monitoring mode
+        NOTE: This method needs aircrack-ng suite
+        Example usage: nttl.enable_monitor(interface='wlan0')
         """
-        pass
+        self.interface = interface
+
+        try:
+            command = "sudo airmon-ng-check kill > /dev/null"
+            system(command)
+            command = f"sudo airmon-ng start {self.interface} > /dev/null"
+            system(command)
+            return True
+        except:
+            print("An error occured while enabling monitor mode")
+            return False
 
     # disabling wireless monitoring - cleaning everything
-    def disable_monitor(self):
+    def disable_monitor(self, interface):
         """
+        Method for disabling wireless monitoring mode
+        Example usage: nttl.disable_monitor(interface='wlan0mon')
+        """
+        self.interface = interface
 
-        """
-        pass
+        try:
+            command = "sudo airmon-ng stop {} > /dev/null".format(self.interface)
+            system(command)
+            command = "sudo systemctl restart NetworkManager.service > /dev/null"
+            system(command)
+            command = "sudo systemctl restart wpa_supplicant.service > /dev/null"
+            system(command)
+            return True
+        except:
+            print("An error occured while disabling monitor mode")
+            return False
 
     # getting device vendor by target's MAC address
     def get_vendor(self, target_MAC):
         """
-
+        Method fot getting information about device vendor by device's MAC address
+        Example usage: nttl.get_vendor(target_MAC='12:ab:34:cd:ef:78')
         """
+        self.target_MAC = target_MAC
+
+        vendor = requests.get("https://api.macvendors.com/{}".format(self.target_MAC))
+        return vendor.text
         
 
     # simulating MAC flood attacks
